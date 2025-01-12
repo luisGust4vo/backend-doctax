cd614e54a1ac2367fc455b8de5a958818d126b4c
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-01-12 01:49:16 -0300

    Feat:criacao do novo modelo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'senha',
-      database: 'nome_do_banco',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Cuidado com isso em produção
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o módulo disponível em toda a aplicação
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Cuidado: desative isso em produção
+      }),
     }),
     UserModule,
   ],
